f6fd92eb1fee8c00b0a70b80bd1caae12c26d234
eitagi12 <supasit.nus@github.com>
2019-05-02 08:53:00 +0700

    Create Exercise4_Supasit_L.py

---
+++ Exercise4_Supasit_L.py
@@ -0,0 +1,10 @@
+foundationeng = 85
+gelbusiness = 94.5
+introcomsystem = 75
+compro = 99
+
+print("---Your Score---")
+print("Foundation English : ",foundationeng)
+print("General Business : ",gelbusiness)
+print("Introduction to Computer system : ",introcomsystem)
+print("Computer Programming : ",compro)
